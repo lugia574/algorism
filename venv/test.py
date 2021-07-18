5c9000c78d7b8868bf49e30304a02f2ef2372e3b
lcw57 <dlcjfdnr789@naver.com>
2021-07-18 15:26:33 +0900

    2021- 07 - 18  1 설탕포대

--- venv/test.py
+++ venv/test.py
@@ -14,19 +14,20 @@
 # ae += str(5+1)
 # print(ae)
 
-apt = [[0 for _ in range(15)] for _ in range(15)]
+# apt = [[0 for _ in range(15)] for _ in range(15)]
+#
+# for i in range(15):
+#     apt[i][1] = 1
+#     apt[0][i] = i
+#
+# for i in range(1, 15):
+#     for j in range(2, 15):
+#         apt[i][j] = apt[i][j - 1] + apt[i - 1][j];
+#
+#
+#
+#
+# for x in apt:
+#     print(x)
 
-for i in range(15):
-    apt[i][1] = 1
-    apt[0][i] = i
-
-for i in range(1, 15):
-    for j in range(2, 15):
-        apt[i][j] = apt[i][j - 1] + apt[i - 1][j];
-
-
-
-
-for x in apt:
-    print(x)
-
+print((11%3)%5)
