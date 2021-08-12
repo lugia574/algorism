5d5439b22c5083e44a945963922084811721970f
lugia574 <dlcjfdnr789@naver.com>
2021-08-12 23:12:30 +0900

    2021-08-12 (2) 재귀 구구단

--- venv/recursive function.py
+++ venv/recursive function.py
@@ -5,4 +5,13 @@ def countdown(m):
         print('카운터',m)
         countdown(m-1)
 
-countdown(10)
+# countdown(10)
+
+def recur_gogodan(m, index = 1):
+    if index == 9:
+        print(m,'*',index,'=',m*index)
+    else:
+        print(m,'*', index, '=', m * index)
+        recur_gogodan(m,index+1)
+
+recur_gogodan(2)
