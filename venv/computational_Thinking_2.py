61b37387c86aa6d53d66c2105582e68703527a72
lugia574 <dlcjfdnr789@naver.com>
2021-11-11 12:09:57 +0900

    2021-11-11 (1) computational_Thinking 2 주석 및 추가

--- venv/computational_Thinking_2.py
+++ venv/computational_Thinking_2.py
@@ -4,8 +4,9 @@
 # 단, 본인을 제외하며, 바로 친구였던 경우도 제외
 
 def relation_cnt(N,relation):
-    rel_rel_num = 0
-    rel_rel_num_index = 0
+    rel_rel_cnt = 0 # 가장 많은 친구의 친구 수
+    rel_rel_cnt_index = 0 # 가장 많은 친구를 가진 번호
+    rel_num = 0 # 가장 많은 친구를 가진 친구를 가진 사람
     nums = []*(N)
 
     # 각 번순의 친구 리스트 만들기
@@ -16,7 +17,9 @@ def relation_cnt(N,relation):
                 rel.append(i[1])
         nums.append(rel)
 
-    print(nums)
+    # 제대로 친구 리스트를 만들었는가 확인
+    # print(nums)
+
     # 친구의 친구 카운팅(len) 하고 조건에 맞는 수 빼서 비교
     for index in range(N):
         minus = 0
@@ -31,12 +34,13 @@ def relation_cnt(N,relation):
                 minus += 1
 
         # 기존 친구의 친구수 랑 비교해서 높으면 값 덮어씌우기
-        if len(nums[index]) - minus > rel_rel_num:
-            rel_rel_num = len(nums[index]) - minus
-            rel_rel_num_index = index+1
+        if len(nums[index]) - minus > rel_rel_cnt:
+            rel_rel_cnt = len(nums[index]) - minus # 가장 많은 친구의 친구 수
+            rel_rel_cnt_index = index+1 # 가장많은 친구 번호
+            rel_num = N # 가장 많은 친구를 가진 친구를 가진 사람
 
 
-    return rel_rel_num , rel_rel_num_index
+    return rel_rel_cnt , rel_rel_cnt_index , rel_num
 
 N = int(input()) # N = 7
 arr = list(map(int,input().split()))
@@ -46,7 +50,10 @@ relation = []
 for i in range(0,len(arr),2):
     relation.append([arr[i],arr[i+1]])
 
-print(relation)
+# 제대로 받아왔는가 확인
+# print(relation)
 
 ans = relation_cnt(N,relation)
-print(ans)
+print('가장 많은 친구의 친구 수: ', ans[0])
+print('가장 많은 친구의 번호: ', ans[1])
+print('가장 많은 친구를 가진 친구를 가진 사람: ', ans[2])
