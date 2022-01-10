83923b27edc0f75df46d6161cb824674ba71aba5
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-01-10 21:31:35 +0900

    2022-01-10 (2) py_algo ~ 12

--- venv/python_algo/test.py
+++ venv/python_algo/test.py
@@ -11,10 +11,22 @@
 # for i in range(10 , 0, -1):
 #     print(i)
 
-for i in range(1, 11):
-    if i%2 == 1:
-        continue
-    print(i)
+# for i in range(1, 11):
+#     if i%2 == 1:
+#         continue
+#     print(i)
+#
+# else:
+#     print("끝났습니당")
 
-else:
-    print("끝났습니당")
+a = [[0]*3]*3
+print(a)
+a[0][1] = 1
+a[0][2] = 2
+
+for i in range(3):
+    for j in range(3):
+        print(a[i][j], end=" ")
+    print()
+
+print(a[1][2])
