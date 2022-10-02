955a678b17ee88518dce95262cc192b9b0100ad5
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-10-02 21:27:27 +0900

    2022-10-2 solution

---
+++ venv/python_algo/code/3. 탐색&시뮬레이션(string, 1차원, 2차원 리스트 탐색 )/algo_303_카드 역배치.py
@@ -0,0 +1,35 @@
+# 카드 역배치(정올 기출)
+
+# 너무 길어 걍 생략
+# 정리하면 1~20 정렬된 리스트에 a~b 까지는 역순으로 재배열하고
+# 그 상태에서 a2 ~ b2 범위를 또 역순으로 재배열 함 이걸 10번 반복함
+
+# ▣ 입력예제 1
+# 5 10
+# 9 13
+# 1 2
+# 3 4
+# 5 6
+# 1 2
+# 3 4
+# 5 6
+# 1 20
+# 1 20
+# ▣ 출력예제 1
+# 1 2 3 4 10 9 8 7 13 12 11 5 6 14 15 16 17 18 19 20
+
+card = [0 + i for i in range(0,21)]
+def Rearrangement (x, y):
+    for i in range((y-x+1)//2):
+        tmp = card[x+i]
+        card[x+i] = card[y-i]
+        card[y-i] = tmp
+
+
+
+for _ in range(10):
+    x, y = map(int ,input().split())
+    Rearrangement(x, y)
+
+for i in range(1,len(card)):
+    print(card[i], end=" ")
