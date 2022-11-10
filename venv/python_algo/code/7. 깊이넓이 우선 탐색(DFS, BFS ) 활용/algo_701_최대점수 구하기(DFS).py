40458412d0643e1cd9df452ab44d17ae9bf7e766
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-11-10 13:42:10 +0900

    2022-11-10 solution

---
+++ venv/python_algo/code/7. 깊이넓이 우선 탐색(DFS, BFS ) 활용/algo_701_최대점수 구하기(DFS).py
@@ -0,0 +1,44 @@
+# 최대점수 구하기(DFS)
+# N개의 문제를 풀려고 합니다. 각 문제는 그것을 풀었을 때 얻는 점수와 푸는데 걸리는 시간이 주어진다
+# 제한시간 M안에 N개의 문제 중 최대점수를 얻을 수 있도록 해야 합니다. (해당문제는
+# 해당시간이 걸리면 푸는 걸로 간주한다, 한 유형당 한개만 풀 수 있습니다.)
+# ▣ 입력설명
+# 첫 번째 줄에 문제의 개수N(1<=N<=20)과 제한 시간 M(10<=M<=300)이 주어집니다.
+# 두 번째 줄부터 N줄에 걸쳐 문제를 풀었을 때의 점수와 푸는데 걸리는 시간이 주어집니다.
+# ▣ 출력설명
+# 첫 번째 줄에 제한 시간안에 얻을 수 있는 최대 점수를 출력합니다.
+# ▣ 입력예제 1
+# 5 20
+# 10 5
+# 25 12
+# 15 8
+# 6 3
+# 7 4
+# ▣ 출력예제 1
+# 41
+def maxPointDFS(l,pSum, tSum):
+    global maxPoint
+
+    if tSum > t:
+        pSum = pSum - problem[l-1][1]
+        if maxPoint < pSum:
+            maxPoint = pSum
+
+    else:
+        for i in range(l, n):
+            if check[i] == 0:
+                check[i] = 1
+                maxPointDFS(i+1, pSum+problem[i][1], tSum+problem[i][2])
+                check[i] = 0
+
+
+if __name__ == "__main__":
+    n,t = map(int,input().split())
+    problem = []
+    for i in range(n):
+        point,time = map(int, input().split())
+        problem.append([i,point,time])
+    maxPoint = -21467000000
+    check = [0]*n
+    maxPointDFS(0, 0, 0)
+    print(maxPoint)
