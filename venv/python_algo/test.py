4010f94b8e347e2f91d49c118e625a7db9b145be
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-11-12 13:20:51 +0900

    2022-11-12 solution

--- venv/python_algo/test.py
+++ venv/python_algo/test.py
@@ -48,11 +48,64 @@
 #     for j in range(5):
 #         testList[i][j] = int(input())
 
-N = int(input())
+# N = int(input())
+#
+# arr = [0 for _ in range(N)]
+#
+# for i in range(N):
+#     arr[i] = list(map(int, input().split()))
+#
+# print(arr[:][0])
 
-arr = [0 for _ in range(N)]
+def fnc(arr, n):
+    res = False
+    numArr = [0] * (n+1)
 
-for i in range(N):
-    arr[i] = list(map(int, input().split()))
+    for x in arr:
+        numArr[x] = 1
 
-print(arr[:][0])
+    for i in range(1, n + 1):
+        if numArr[i] == 0:
+            if i == k:
+                return res
+            else:
+                break
+    resArr.append(arr)
+    res = True
+    return res
+
+
+
+n = int(input())
+k = int(input())
+arr = list(map(int,input().split(",")))
+resArr = []
+cnt = 0
+for i in range(n):
+    for j in range(i+1,n+1):
+        res = fnc(arr[i:j],n)
+        if res == True:
+            cnt += 1
+print(cnt)
+print(resArr)
+
+#
+# 10
+# 4
+# 8,9,4,6,5,1,2,3,10,7
+#
+# 7
+# 3
+# 7,5,1,4,6,2,3
+#
+# 4
+# 3
+# 3,2,1,4
+#
+# 15
+# 6
+# 7,8,5,14,2,11,4,3,15,1,9,10,13,6,12
+#
+# 20
+# 8
+# 8,14,13,15,20,12,10,2,7,16,6,1,3,5,4,17,18,19,11,9
