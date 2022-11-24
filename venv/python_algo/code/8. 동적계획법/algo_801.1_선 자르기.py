d1fea1016206bbc91ee5f16b7aaf46ad8e48d461
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-11-24 15:11:39 +0900

    2022-11-24 solution

--- venv/python_algo/code/8. 동적계획법/algo_801.1_선 자르기.py
+++ venv/python_algo/code/8. 동적계획법/algo_801.1_선 자르기.py
@@ -27,11 +27,23 @@ def DFS(l):
             if l-i >-1:
                 DFS(l-i)
 
+# 이렇게 하면 이제 n 값이 커지기 시작하면 존나 재귀를 돌아버려서 타임 아웃이 되어버림
+# 즉 점화식으로 풀어야해
+# 해당 문제는 f(n) = f(n-2) + f(n-1) 의 식을 가짐
+# 그러니까 7 의 값은 f(7) = f(5) + f(6) 의 값을 가진다는 거지
+# 이걸 쭉 내려가면 f(1), f(2) 값만 알면 쉽게 값을 구할 수 있다 이말이야
 
+def f(n):
+    dy[n] = dy[n-1] + dy[n-2]
 
 if __name__ == "__main__":
     n = int(input())
     cnt = 0
-    DFS(n)
-    print(cnt)
+    dy = [0] * (n+1)
+    dy[1] = 1
+    dy[2] = 2
+    for i in range(3, n+1):
+        f(i)
+    # DFS(n)
+    print(dy[n])
 
