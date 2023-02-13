9912b4a3d6347499c74f1bfb47980b873339629c
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-02-13 15:58:38 +0900

    2023 -02- 03 codeing

--- venv/python_algo/code3/l2-이진 변환 반복하기.py
+++ venv/python_algo/code3/l2-이진 변환 반복하기.py
@@ -8,6 +8,7 @@
 # 0과 1로 이루어진 문자열 s가 매개변수로 주어집니다.
 # s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때,
 # 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+
 def solution_sol(s):
     a, b = 0, 0
     while s != '1':
@@ -17,10 +18,17 @@ def solution_sol(s):
         s = bin(num)[2:]
     return [a, b]
 
+def binaryScale(len):
+    result = []
+    while len != 0:
+        if len % 2 == 1:
+            result.insert(0, "1")
+            len = (len-1)/2
+        else:
+            result.insert(0, "0")
+            len = len/2
+    return result
 
-def binaryScale(lenght):
-    ans = list(bin(lenght))[2:]
-    return ans
 
 def solution(s):
     conversion = 0
