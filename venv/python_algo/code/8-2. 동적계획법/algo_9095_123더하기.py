382b107f868d4cab21c652dda1847ce1d1e1c959
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-03-09 15:58:02 +0900

    2023  codeing

---
+++ venv/python_algo/code/8-2. 동적계획법/algo_9095_123더하기.py
@@ -0,0 +1,28 @@
+import sys
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        if n == 1 or n == 2:
+            print(n)
+            continue
+        dp = [0] * (n + 1)
+
+        # 알고리즘 ㄱ
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+
+        # dp[4] = 1111, 121, 112, 211, 31, 13, 22 > 7 > 1 + 2 + 4
+        # dp[5] = 111/11, 2111, 1211, 1121, 1112, 311, 131, 113, 122, 212, 221, 23, 32 > 13 >  2 + 4 + 7
+        if n < 4:
+            print(dp[n])
+            continue
+
+        for i in range(4, n+1):
+            dp[i] = dp[i-3] + dp[i-2] + dp[i - 1]
+        print(dp[n])
+
+
+
