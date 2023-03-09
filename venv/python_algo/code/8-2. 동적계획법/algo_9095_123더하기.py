ca90b75d701ddbcdf02dfcd86762b90fe4a48b28
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-03-09 16:02:14 +0900

    2023  codeing

--- venv/python_algo/code/8-2. 동적계획법/algo_9095_123더하기.py
+++ venv/python_algo/code/8-2. 동적계획법/algo_9095_123더하기.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
         if n == 1 or n == 2:
             print(n)
             continue
+        elif n == 3:
+            print(4)
         dp = [0] * (n + 1)
 
         # 알고리즘 ㄱ
@@ -16,9 +18,6 @@ if __name__ == "__main__":
 
         # dp[4] = 1111, 121, 112, 211, 31, 13, 22 > 7 > 1 + 2 + 4
         # dp[5] = 111/11, 2111, 1211, 1121, 1112, 311, 131, 113, 122, 212, 221, 23, 32 > 13 >  2 + 4 + 7
-        if n < 4:
-            print(dp[n])
-            continue
 
         for i in range(4, n+1):
             dp[i] = dp[i-3] + dp[i-2] + dp[i - 1]
