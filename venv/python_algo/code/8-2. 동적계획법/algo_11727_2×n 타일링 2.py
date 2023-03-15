e293f460bce41dfe2c82f0c1a1dc11bb722d401d
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-03-15 19:04:56 +0900

    2023  codeing

---
+++ venv/python_algo/code/8-2. 동적계획법/algo_11727_2×n 타일링 2.py
@@ -0,0 +1,15 @@
+# 이번에는 2×2 이 추가되서
+# 1×2, 2×1과 2×2 타일로 채우는 방법의 수
+
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    dp = [0] * 1001
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + (dp[i-2] * 2)) % 10007
+
+    print(dp[n])
