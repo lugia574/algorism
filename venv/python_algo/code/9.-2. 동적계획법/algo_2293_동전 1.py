2c3cfd9b387f354dbff982ec4a504c4bed351bf2
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-03-17 21:24:07 +0900

    2023  codeing

--- venv/python_algo/code/9.-2. 동적계획법/algo_2293_동전 1.py
+++ venv/python_algo/code/9.-2. 동적계획법/algo_2293_동전 1.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     coin = []
     for _ in range(n):
         coin.append(int(input()))
+    dp = [0] * (k+1)
 
-
-    # 1     1
-    # 2     11      2
-    # 3     111     21
-    # 4     1111    211 22
-    # 5     11111   2111 221     5
-    #
+    dp[0] = 1
+    for i in coin:
+        for j in range(1, k+1):
+            if j - i >= 0:
+                dp[j] += dp[j-i]
+        #print(dp)
+    print(dp[k])
