b2f7e7fc53b0be7a67c466f754c3108e9d2c95d4
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-02 16:54:03 +0900

    2023  codeing

--- venv/python_algo/code/5-2. 자료구조 활용/algo_10828_스택.py
+++ venv/python_algo/code/5-2. 자료구조 활용/algo_10828_스택.py
@@ -1,30 +1,64 @@
 import sys
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        pop_object = None
+        if self.isEmpty():
+            print("Stack is Empty")
+        else:
+            pop_object = self.stack.pop()
+
+        return pop_object
+
+    def top(self):
+        top_object = None
+        if self.isEmpty():
+            print("Stack is Empty")
+        else:
+            top_object = self.stack[-1]
+
+        return top_object
+
+    def isEmpty(self):
+        is_empty = False
+        if len(self.stack) == 0:
+            is_empty = True
+        return is_empty
+
+    def size(self):
+        return len(self.stack)
+
 
 if __name__ == "__main__":
     input = sys.stdin.readline
     n = int(input())
 
-    stack = []
+    stackArr = Stack()
     for _ in range(n):
         command = sys.stdin.readline().split()
 
         if command[0] == 'push':
-            stack.append(command[1])
+            stackArr.push(command[1])
         elif command[0] == 'pop':
-            if len(stack) == 0:
+            if stackArr.isEmpty():
                 print(-1)
             else:
-                print(stack.pop())
+                print(stackArr.pop())
         elif command[0] == 'size':
-            print(len(stack))
+            print(stackArr.size())
         elif command[0] == 'empty':
-            if len(stack) == 0:
+            if stackArr.size() == 0:
                 print(1)
             else:
                 print(0)
         elif command[0] == 'top':
-            if len(stack) == 0:
+            if stackArr.size() == 0:
                 print(-1)
             else:
-                print(stack[-1])
+                print(stackArr.top())
