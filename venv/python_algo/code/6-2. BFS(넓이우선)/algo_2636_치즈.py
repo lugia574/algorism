37485797c739e288e8836514dd15c781135d1658
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-11 15:25:31 +0900

    2023  codeing

---
+++ venv/python_algo/code/6-2. BFS(넓이우선)/algo_2636_치즈.py
@@ -0,0 +1,49 @@
+# 대체 치즈 안의 구멍을 어떻게 구분하냐 존나 모르겠었는데
+# 다 방법이 있더라 시벌
+import math
+import sys
+from collections import deque
+
+def BFS(a, b):
+    q.append((a, b))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c and visited[nx][ny] == 0:
+                if board[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+                else:
+                    visited[nx][ny] = 2
+                    outlineCheese.append((nx, ny))
+
+    for cx, cy in outlineCheese:
+        board[cx][cy] = 0
+
+    return len(outlineCheese)
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    r, c = map(int, input().split())
+    board = [list(map(int,input().split())) for _ in range(r)]
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+    day = -1
+    cheese = math.inf
+    while True:
+        day += 1
+        q = deque()
+        outlineCheese = deque()
+        visited = [[0] * c for _ in range(r)]
+        tmp = BFS(0,0)
+
+        if tmp == 0:
+            break
+        else:
+            cheese = tmp
+
+    print(day)
+    print(cheese)
