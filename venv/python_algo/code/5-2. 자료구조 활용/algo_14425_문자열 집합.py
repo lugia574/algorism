da6ecabcbac7459f854ba93fe91a2848d4cc3111
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-19 17:06:00 +0900

    2023  codeing

---
+++ venv/python_algo/code/5-2. 자료구조 활용/algo_14425_문자열 집합.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    group = set()
+    cnt = 0
+    for _ in range(n):
+        s = input()
+        group.add(s)
+
+    for _ in range(m):
+        ss = input()
+        if ss in group:
+            cnt += 1
+
+    print(cnt)
