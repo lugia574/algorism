bcead3c08af5bc138f35bac12fc0bf10613fa73c
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-20 16:48:07 +0900

    2023  codeing

---
+++ venv/python_algo/code/9-3. 동적계획법/algo_11060_점프점프.py
@@ -0,0 +1,17 @@
+import sys, math
+from collections import deque
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    a = list(map(int, input().split()))
+    cnt = 1
+    dp = [n+1] * n
+    dp[0] = 0
+    for i in range(n):
+        for j in range(1, a[i] + 1):
+            if i + j >= n: break
+            dp[i+j] = min(dp[i+j], dp[i]+1)
+
+    print(dp[n-1] if dp[n-1] != n+1 else -1)
+
