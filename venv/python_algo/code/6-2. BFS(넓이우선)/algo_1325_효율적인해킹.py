049019f4bdd54ed951d08143057340605f5357b4
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-30 20:24:27 +0900

    2023  codeing

---
+++ venv/python_algo/code/6-2. BFS(넓이우선)/algo_1325_효율적인해킹.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+def BFS(b):
+    q = deque()
+    q.append(b)
+    check = [0] * (n+1)
+    check[b] = 1
+    while q:
+        x = q.popleft()
+        for y in node[x]:
+            if check[y] == 0:
+                q.append(y)
+                check[y] = 1
+    return sum(check)
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n, m = map(int, input().rsplit())
+    node = [[] for _ in range (n+1)]
+    r = [list(map(int, input().rsplit())) for _ in range(m)]
+    dp = [0] * (n+1)
+    res = []
+    maxCnt = 0
+
+    for a, b in r:
+        node[b].append(a)
+
+    for i in range(1, n+1):
+        if node[i]:
+            cnt = BFS(i)
+            if cnt > maxCnt:
+                maxCnt = cnt
+                res = [i]
+            elif cnt == maxCnt:
+                res.append(i)
+    print(*res)
