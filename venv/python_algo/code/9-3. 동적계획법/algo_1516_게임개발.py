24ff87056d4c4975916ba5f5c4bae28829473f5f
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-31 18:19:24 +0900

    2023  codeing

--- venv/python_algo/code/9-3. 동적계획법/algo_1516_게임개발.py
+++ venv/python_algo/code/9-3. 동적계획법/algo_1516_게임개발.py
@@ -1,3 +1,10 @@
+# 참내 이걸 주석을 안달았네
+# 이거 만약에 다시 풀면 나 100퍼 못풀텐데 뭔 자신감으로 안했데? ㅋ
+# 자 값을 하나씩 받아
+# 여기서 뭐를 사전에 해야한다는게 없는 바로 -1 상태면 바로 dp에 박아버려
+# 사전에 필요한것들이 있는 것들은 하나하나 보면서 해당 노드가 필요로하는 노드들을 체크하고 해당 노드는 엣지가 몇개인지도 세
+# 그리고 이제 while 문을 도는거야 que 하나씩 빼면서
+#
 import sys
 from collections import deque
 
@@ -20,7 +27,7 @@ def simCity(n):
                 break
             seq[x-1].append(i)
             inDegree[i] += 1
-
+    # print(seq)
     while q:
         b = q.popleft()
         for x in seq[b]:
