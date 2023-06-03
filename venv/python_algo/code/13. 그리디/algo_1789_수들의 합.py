61ca92716eb7f652cfa80db744b31b8ace309ef8
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-06-03 14:16:07 +0900

    2023  codeing

---
+++ venv/python_algo/code/13. 그리디/algo_1789_수들의 합.py
@@ -0,0 +1,14 @@
+
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    s = int(input())
+
+    n = 1
+    while True:
+        if (n * (n+1)) / 2 > s:
+            break
+        else:
+            n += 1
+    print(n-1)`
