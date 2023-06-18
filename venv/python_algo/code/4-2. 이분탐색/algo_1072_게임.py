96c787a15bea60d5096705e10cfa3e16b1b861f2
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-06-18 13:48:26 +0900

    2023  codeing

---
+++ venv/python_algo/code/4-2. 이분탐색/algo_1072_게임.py
@@ -0,0 +1,28 @@
+import sys
+
+def winrate(x, y):
+    z = (y * 100) // x
+    if z >= 99:
+        return -1
+
+    rt = x
+    lt = 1
+    answer = 0
+
+    while lt <= rt:
+        mid = (rt + lt) // 2
+        midAvg = (y + mid) * 100 // (x+mid)
+        if midAvg <= z:
+            lt = mid + 1
+        else:
+            answer = mid
+            rt = mid - 1
+
+    return answer
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    x, y = map(int, input().split())
+    answer = winrate(x, y)
+    print(answer)
+
+
