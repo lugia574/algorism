d21513cea2a98f76559ab39b164ba04fc2dc7a0b
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-07-17 21:15:12 +0900

    2023  codeing

---
+++ venv/python_algo/code/5-3. 자료구조 활용/algo_11652_카드.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    card_dic = {}
+
+    for i in range(n):
+        card = int(input())
+        if card in card_dic:
+            card_dic[card] += 1
+        else:
+            card_dic[card] = 1
+
+    result = sorted(card_dic.items(), key=lambda x: (-x[1], x[0]))
+    print(result[0][0])
+
