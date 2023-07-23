5ebc87a11d5e332ae0842985a22468b5fa3ffeed
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-07-23 13:59:30 +0900

    2023  codeing

---
+++ venv/python_algo/code/2-2. 구현/algo_1316_그룹 단어 체커.py
@@ -0,0 +1,25 @@
+import sys
+
+def groupChecker(st):
+    dic = []
+    pre = st[0]
+    for x in st[1:]:
+        if pre == x:
+            continue
+        else:
+            if x in dic:
+                return False
+            dic.append(pre)
+            pre = x
+    return True
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    answer = 0
+    for _ in range(n):
+        st = input().strip()
+        if groupChecker(st):
+            answer += 1
+
+    print(answer)
