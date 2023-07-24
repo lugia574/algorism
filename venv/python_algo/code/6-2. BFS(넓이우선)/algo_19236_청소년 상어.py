2f6f26f0e2b451e1ceafde5ef607725f96bcaf30
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-07-24 22:33:57 +0900

    2023  codeing

--- venv/python_algo/code/6-2. BFS(넓이우선)/algo_19236_청소년 상어.py
+++ venv/python_algo/code/6-2. BFS(넓이우선)/algo_19236_청소년 상어.py
@@ -8,89 +8,132 @@
 # 하 그럼 어떻게 하지?
 # 그냥 심심해서 q 에 다 박고 시작한게 아닌데
 # 이럼 매번 board 를 돌면서 해당 시작 번호를 찾아야한다는 소리 아님??
-import sys; input = sys.stdin.readline
-import copy
+# 와 해당 코드 찾아 보는데 하나하나 다 찾네 ㅋㅋㅋㅋ 1번 물고기 16번 돌고 ㅋㅋ 2번 물고기 16번 돌고 ㅋㅋㅋ
+# 물론 최악의 경우에도 1억 코드가 안되긴하는데
+# 개 무식하네ㅋㅋ 너무 하드해서 자바는 어떤가 찾아봄
+# 자바는 Fish 클래스를 만들어 ArrayList 에 한데 박아서 관리함
+# 클래스 안에 x, y 좌표 다 저장해져 있고, 살았냐 죽었냐 체크도 되어 있으니
+# 0 부터 15 (1 ~ 16번) index 로 정렬해서 ArrayList.get 으로 불러내면 됨
+# 먹힌 새끼도 죽었다라고 체크만 하는거지 ArrayList 에서 사라지는게 아니기때문에 index 가 꼬일것도 없음
+# https://bcp0109.tistory.com/215
+# 차라리 이 방법을 이용해서 파이썬을 다시 작성하는게 도움 될듯? 이게 훨 깔끔함
+# 역시 좀만 복잡해져도 그냥 class 로 만들고 ArrayList 로 관리하는게 좋은듯
+import sys, copy
+input = sys.stdin.readline
 
-# 물고기 좌표 찾는 함수
-def find_fish(graph, fish):
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j][0] == fish:
-                return (i, j)
+class Shark:
+    def __init__(self, x, y, dir, eatSum):
+        self.x = x
+        self.y = y
+        self.dir = dir
+        self.eatSum = eatSum
 
-# 모든 물고기 이동시키는 함수
-def move_fish(x_shark, y_shark, graph):
-    # 번호가 낮은 물고기부터 순차 이동
-    for fish in range(1, 17):
-        # 물고기 좌표 찾기
-        position = find_fish(graph, fish)
-        # 해당 물고기가 살아있는 경우
-        if position:
-            x_fish, y_fish = position[0], position[1] # 좌표 리턴받기
-            direction = graph[x_fish][y_fish][1]
-            # 반시계 방향으로 45도씩 최대 360도(1바퀴)까지 회전
-            for _ in range(len(d)):
-                # 해당 방향으로 진행
-                nx_fish = x_fish + d[direction][0]
-                ny_fish = y_fish + d[direction][1]
-                # 맵 내부 위치한 경우
-                if 0 <= nx_fish < N and 0 <= ny_fish < N:
-                    # 진행할 곳에 상어가 없는 경우
-                    if not (nx_fish == x_shark and ny_fish == y_shark):
-                        # 해당 방향을 진행방향으로 확정
-                        graph[x_fish][y_fish][1] = direction
-                        # 물고기 간 위치 변경
-                        graph[nx_fish][ny_fish], graph[x_fish][y_fish] = graph[x_fish][y_fish], graph[nx_fish][ny_fish]
-                        break # 진행방향이 확정되었기 때문에 진행 방향을 더 이상 바꿀 필요 없음
-                direction = (direction + 1) % len(d)
 
-# 상어의 이동가능한 좌표 찾는 함수
-def get_movable_position(x_shark, y_shark, graph):
-    direction = graph[x_shark][y_shark][1] # 상어 진행방향
-    position = []
-    # 최대 (맵 크기 -1)까지 이동 가능
-    for _ in range(N-1):
-        # 진행방향으로 전진
-        x_shark += d[direction][0]
-        y_shark += d[direction][1]
-        # 진행 후 맵 내부에 위치해 있으며 물고기가 존재하는 경우
-        if 0 <= x_shark < N and 0 <= y_shark < N and graph[x_shark][y_shark][0] != -1:
-            position.append((x_shark, y_shark))
-    return position
+class Fish:
+    def __init__(self, x, y, id, dir, isAlive):
+        self.x = x
+        self.y = y
+        self.id = id
+        self.dir = dir
+        self.isAlive = isAlive
 
-# 물고기를 모두 먹을 때까지 물고기와 상어를 이동시키는 재귀함수
-def dfs(x_shark, y_shark, eat, graph):
-    global answer
-    graph = copy.deepcopy(graph)
-    # 상어가 해당 물고기 잡아먹음
-    eat += graph[x_shark][y_shark][0]
-    graph[x_shark][y_shark][0] = -1 # 해당 위치 물고기 잡아먹힘 표시
-    move_fish(x_shark, y_shark, graph) # 모든 물고기 이동
-    # 상어의 이동가능한 좌표(=물고기 위치) 리턴받기
-    position = get_movable_position(x_shark, y_shark, graph)
 
-    # 이동가능한 좌표가 남은 경우
-    if position:
-        for nx_shark, ny_shark in position:
-            dfs(nx_shark, ny_shark, eat, graph)
-    else:
-        answer = max(answer, eat)
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]
+maxSum = 0
+
+
+def main():
+    arr = [[0 for _ in range(4)] for _ in range(4)]
+    fishes = []
+
+    for i in range(4):
+        line = list(map(int, input().split()))
+        for j in range(4):
+            f = Fish(i, j, line[j * 2], line[j * 2 + 1] - 1, True)
+            fishes.append(f)
+            arr[i][j] = f.id
+
+    fishes.sort(key=lambda x: x.id)
+
+    f = fishes[arr[0][0] - 1]
+    shark = Shark(0, 0, f.dir, f.id)
+    f.isAlive = False
+    arr[0][0] = -1
+
+    dfs(arr, shark, fishes)
+    print(maxSum)
+
+
+def dfs(arr, shark, fishes):
+    global maxSum
+
+    if maxSum < shark.eatSum:
+        maxSum = shark.eatSum
+
+    for fish in fishes:
+        move_fish(fish, arr, fishes)
+
+    for dist in range(1, 4):
+        nx = shark.x + dx[shark.dir] * dist
+        ny = shark.y + dy[shark.dir] * dist
+
+        if 0 <= nx < 4 and 0 <= ny < 4 and arr[nx][ny] > 0:
+            arrCopies = copy_arr(arr)
+            fishCopies = copy_fishes(fishes)
+
+            arrCopies[shark.x][shark.y] = 0
+            f = fishCopies[arr[nx][ny] - 1]
+            new_shark = Shark(f.x, f.y, f.dir, shark.eatSum + f.id)
+            f.isAlive = False
+            arrCopies[f.x][f.y] = -1
+
+            dfs(arrCopies, new_shark, fishCopies)
+
+
+def move_fish(fish, arr, fishes):
+    if not fish.isAlive:
         return
 
-if __name__ == '__main__':
-    N = 4
-    graph = [[None]*N for _ in range(N)]
-    for i in range(N):
-        data = list(map(int, input().split()))
-        for j in range(N):
-            # 물고기 번호, 방향 정보 저장
-            # 방향 정보 값에 1을 뺀 이유: 진행방향을 저장한 리스트(d)의 값이 인덱스 0부터 시작하기 떄문
-            graph[i][j] = [data[2*j], data[2*j+1]-1]
+    for i in range(8):
+        nextDir = (fish.dir + i) % 8
+        nx = fish.x + dx[nextDir]
+        ny = fish.y + dy[nextDir]
 
-    # 진행방향: 상, 좌상, 좌, 좌하, 하, 우하, 우, 우상
-    d = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
-    # (0, 0)에서 시작
-    x_shark = y_shark = 0
-    answer = 0
-    dfs(x_shark, y_shark, 0, graph)
-    print(answer)
+        if 0 <= nx < 4 and 0 <= ny < 4 and arr[nx][ny] > -1:
+            arr[fish.x][fish.y] = 0
+
+            if arr[nx][ny] == 0:
+                fish.x = nx
+                fish.y = ny
+            else:
+                temp = fishes[arr[nx][ny] - 1]
+                temp.x = fish.x
+                temp.y = fish.y
+                arr[fish.x][fish.y] = temp.id
+
+                fish.x = nx
+                fish.y = ny
+
+            arr[nx][ny] = fish.id
+            fish.dir = nextDir
+            return
+
+
+def copy_arr(arr):
+    temp = [[0 for _ in range(4)] for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            temp[i][j] = arr[i][j]
+    return temp
+
+
+def copy_fishes(fishes):
+    temp = []
+    for fish in fishes:
+        temp.append(Fish(fish.x, fish.y, fish.id, fish.dir, fish.isAlive))
+    return temp
+
+
+if __name__ == "__main__":
+    main()
