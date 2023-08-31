0782c9a7a7e19fd199ee60683fe10cd222d2d7a3
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-08-31 17:49:53 +0900

    2023  codeing

---
+++ venv/python_algo/code/15. 다익스트라/algo_4485_녹색옷 젤다.py
@@ -0,0 +1,41 @@
+# 따아악 다익스트라에 어울리는 최소로 어쩌구 내용이네
+
+import sys, heapq
+
+def dijkstra():
+    heap = []
+    heapq.heappush(heap, [board[0][0], 0, 0])
+
+    while heap:
+        point, x, y = heapq.heappop(heap)
+        if x == n-1 and y == n-1:
+            return point
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 > nx or nx >= n or 0 > ny or ny >= n or visited[nx][ny]: continue
+            visited[nx][ny] = True
+            newPoint = point + board[nx][ny]
+            heapq.heappush(heap, [newPoint, nx, ny])
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    idx = 1
+    while True:
+        n = int(input())
+        if n == 0: break
+        board = [list(map(int, input().split())) for _ in range(n)]
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+        visited = [[False] * n for _ in range(n)]
+        visited[0][0] = True
+        res = dijkstra()
+        print("Problem %d: %d"%(idx,res))
+        idx += 1
+
+# 3
+# 5 5 4
+# 3 9 1
+# 3 2 7
+# 0
